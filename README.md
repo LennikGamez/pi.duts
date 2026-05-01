d2d7a920a0bc84f2a6b7f45df050c2670ed5009c
aws1313 <69417093+aws1313@users.noreply.github.com>
2026-05-01 21:21:10 +0200

    add example models

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 - [ ] authentication with direct login 
 - [ ] get current courses, sync announcements and make them available via RSS
 - [ ] sync files per course in specific directory structure
-- [ ] system to track homework (with due date, participants)
+- [ ] system to track homework (with due date, participants, ...)
+- [ ] containerized version for usage with nextcloud, syncthing or webserver
 
 ## DISCLAIMER
 This project is not affiliated with stud.ip.
 
 Use on you own risk.
 
-This is not vibe coded.
+This project is not vibe coded.
---
+++ src/models.py
@@ -0,0 +1,43 @@
+from typing import List
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+
+
+class DataBase(DeclarativeBase):
+    pass
+
+class User(DataBase):
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column()
+    base_url: Mapped[str] = mapped_column()
+    sync_dir: Mapped[str] = mapped_column()
+
+class Course(DataBase):
+    __tablename__ = 'course'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    display_name: Mapped[str] = mapped_column()
+
+    announcements: Mapped[List["Announcement"]] = relationship(back_populates="course")
+    file: Mapped[List["File"]] = relationship(back_populates="course")
+
+
+class Announcement(DataBase):
+    __tablename__ = 'announcement'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column()
+    content: Mapped[str] = mapped_column()
+
+    course: Mapped["Course"] = relationship(back_populates="announcements")
+
+
+class File(DataBase):
+    __tablename__ = 'file'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    display_name: Mapped[str] = mapped_column()
+    course: Mapped["Course"] = relationship(back_populates="files")
