09fda2c8ecd52d9025b9c6eb0f875c1dc7c9f47f
Leander <leander@doerk.org>
2026-05-07 19:30:55 +0200

    root_dir parameter to set the download root folder for cloning a folder structure

--- src/file_downloader.py
+++ src/file_downloader.py
@@ -9,24 +9,12 @@ FILE_FLAT_PAGE = "/dispatch.php/course/files/flat?cid="
 DOWNLOAD_NEWEST_FILES = "/dispatch.php/course/files/newest_files?cid="
 
 
-def clone_folder_structure_of_course(navigator: Navigator, cid: str):
+def clone_folder_structure_of_course(navigator: Navigator, cid: str, root_dir="./downloads"):
     url = navigator.base_url + FILE_DIRECTORY_PAGE + cid
-    clone_folder(navigator, url)
+    clone_folder(navigator, url, root_dir=root_dir)
     print("Done!")
 
-            
-        
-
-def download_file(navigator: Navigator, url, file_name, file_location="./downlods"):
-    response = navigator.get_url(url)
-    directories = path.dirname(path.join(file_location, file_name))
-    if not path.exists(directories):
-        makedirs(directories)
-
-    with open(path.join(file_location, file_name), "wb") as f:
-        f.write(response.content)
-
-def clone_folder(navigator: Navigator, url, sub_folder=""): 
+def clone_folder(navigator: Navigator, url, root_dir="./downloads", sub_folder=""): 
     response = navigator.get_url(url)
     soup = BeautifulSoup(response.content, "html.parser")
     form = soup.find(id="files_table_form")
@@ -37,11 +25,19 @@ def clone_folder(navigator: Navigator, url, sub_folder=""):
 
     for file in data_files:
         # yield file
-        download_file(navigator, file.get("download_url"), path.join(sub_folder, file.get("name")))
+        download_file(navigator, file.get("download_url"), path.join(sub_folder, file.get("name")), root_dir=root_dir)
         # here could be a potential DB Update
         print(f"{file.get('name')} has been downloaded!")
     for folder in data_folders:
         # yield {"folder": folder, "files": list(clone_folder(navigator, folder.get("url")))}
-        clone_folder(navigator, folder.get("url"), path.join(sub_folder, folder.get("name")))
+        clone_folder(navigator, folder.get("url"), root_dir, path.join(sub_folder, folder.get("name")))
         # here could be a potential DB Update
         
+def download_file(navigator: Navigator, url, file_name, root_dir="./downloads"):
+    response = navigator.get_url(url)
+    directories = path.dirname(path.join(root_dir, file_name))
+    if not path.exists(directories):
+        makedirs(directories)
+
+    with open(path.join(root_dir, file_name), "wb") as f:
+        f.write(response.content)
