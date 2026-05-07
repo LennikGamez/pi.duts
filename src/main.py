81c0cd61036f20a4b83fef1f7962f8f5b95f6bd3
Leander <leander@doerk.org>
2026-05-07 19:33:01 +0200

    example for using course function and file function together

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from courses import get_list_of_courses
 from file_downloader import clone_folder_structure_of_course
 from navigator import Navigator
 from login import get_tokens_from_login_page, login
@@ -11,4 +12,5 @@ security_token, login_ticket = get_tokens_from_login_page(nav)
 res = login(nav, security_token, login_ticket, username, password)
 print("You are now logged in :)")
 
-clone_folder_structure_of_course(nav, "18cafcf4d0bd83fdb9b40206c70ac8df")
+courses = list(get_list_of_courses(nav))
+clone_folder_structure_of_course(nav, courses[0]["id"], root_dir="./downloads/"+courses[0]["name"])
